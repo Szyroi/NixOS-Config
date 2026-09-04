74428188ccba2b3597b08ef3ce75d9a18102c8c9
Szyroi <pixelcraft696@gmail.com>
2026-09-04 22:17:17 +0200

    Changes: Removed IMA from Framework

--- devices/framework-laptop.nix
+++ devices/framework-laptop.nix
@@ -1,42 +1,10 @@
-{
-  lib,
-  username,
-  ...
-}: {
+{username, ...}: {
   home-manager.users.${username}.imports = [
     ../modules/home-manager/desktop/hyprland/framework-laptop.nix
   ];
 
   hardware.cpu.amd.updateMicrocode = true;
   services.xserver.videoDrivers = ["amdgpu"];
-
-  boot.kernelPatches = [
-    {
-      name = "ima";
-      patch = null;
-
-      structuredExtraConfig = with lib.kernel; {
-        INTEGRITY = yes;
-
-        IMA = yes;
-        IMA_LSM_RULES = yes;
-        IMA_NG_TEMPLATE = yes;
-
-        CRYPTO_SHA512 = yes;
-
-        IMA_READ_POLICY = yes;
-
-        # Do not enable appraisal/EVM yet.
-        #
-        # IMA_APPRAISE = yes;
-        # EVM = yes;
-      };
-    }
-  ];
-
-  boot.kernelParams = [
-    "ima_hash=sha256"
-  ];
 
   services.fprintd.enable = true;
   security.pam.services = {
